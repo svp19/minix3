f741492cd33a35d812a261d8ac19af618a6d25c2
neuralflux <anudeep.tumati99@gmail.com>
2020-03-06 04:12:31 +0530

    sharedmem

--- lab5/transform_3edge.cpp
+++ lab5/transform_3edge.cpp
@@ -11,76 +11,33 @@
 #define MAX 4096
 #define vvi vector<vector<int>>
 #define vi vector<int>
-#define SNAME "/imagesem"
+#define SNAME_READ "/readimage"
+#define SNAME_WRITE "/writeimage"
 
 using namespace std;
 
-sem_t *sem = sem_open(SNAME, O_CREAT, 0644, 0);
+sem_t *write_sem;
+sem_t *read_sem;
+
 
 class Image
 {
   public:
-    int** gray;
-    int ** r;
-    int ** g;
-    int ** b;
     int ** edges;
-    int h, w, new_h, new_w;
+    int h, w;
     string file_name;
 
     Image(){
        h = w = 0;
     }
 
-    Image(string file_name){
-        this->file_name = file_name;
-        readImage(file_name);
-    }
-
-    void readImage(string file_name) {
-
-        // Get file pointer
-        ifstream fin(file_name);
-        string first_line;
-        int val;
-
-        // Read header
-        fin >> first_line;
-        fin >> this->w;
-        fin >> this->h;
-        fin >> val;
-
-        // Dynamically allocate all 2D Arrays
-        gray = new int*[h];
+    void allocateMatrix() {
+        // Dynamically allocate the 2D Array
         edges = new int*[h];
-        r = new int*[h];
-        g = new int*[h];
-        b = new int*[h];
         for(int i=0; i<h; ++i){
-            gray[i] = new int[w];
             edges[i] = new int[w];
-            r[i] = new int[w];
-            g[i] = new int[w];
-            b[i] = new int[w];
-        }
-
-        // Read pixel values
-        for(int i=0; i < h; ++i) {
-            for(int j=0; j < w; ++j) {
-                fin >> r[i][j] >> g[i][j] >> b[i][j];
-            }
-        }
-        fin.close();
-    }
-
-    void grayScale() {
-        for(int i=0; i < h; ++i) {
-            for(int j = 0; j < w; ++j){
-                gray[i][j] = 0.3*r[i][j] + 0.59*g[i][j] + 0.11*b[i][j];
-            }
         }
     }
-
 
     void detectEdges() {
         int filter[3][3] = {
@@ -89,24 +46,38 @@ class Image
             {1, 2, 1}
         };
 
-        new_h = h - 2;
-        new_w = w - 2;
-
-        for(int i = 0; i < h; ++i){
-            for(int j = 0; j < w; ++j){
-                int pixel = 0;
-                for(int x = 0; x<3; ++x){
-                    for(int y = 0; y<3; ++y){
-                        if(i + x >= h || j + y >= w)
-                            continue;
-                        pixel += gray[i+x][j+y] * filter[x][y];
-                    }
-                }
-                edges[i][j] = max(min(pixel, 255), 0);
+        key_t my_key = ftok("shmfile", 65); // ftok function is used to generate unique key
+        int shmid = shmget(my_key, sizeof(int), 0666|IPC_CREAT); // shmget returns an ide in shmid
+        int *pixel = (int*) shmat(shmid,(void*)0,0); // shmat to join to shared memory
+        
+        for(int i=0; i<10; ++i){
+            for(int j=0; j < 10; ++j){
+                sem_wait(write_sem);
+                cout << *pixel << "\n";
+                sem_post(read_sem);
             }
         }
+        shmdt(pixel);
+        shmctl(shmid,IPC_RMID,NULL); // destroy the shared memory
 
-        writeToFile(file_name);
+        // new_h = h - 2;
+        // new_w = w - 2;
+
+        // for(int i = 0; i < h; ++i){
+        //     for(int j = 0; j < w; ++j){
+        //         int pixel = 0;
+        //         for(int x = 0; x<3; ++x){
+        //             for(int y = 0; y<3; ++y){
+        //                 if(i + x >= h || j + y >= w)
+        //                     continue;
+        //                 pixel += gray[i+x][j+y] * filter[x][y];
+        //             }
+        //         }
+        //         edges[i][j] = max(min(pixel, 255), 0);
+        //     }
+        // }
+
+        // writeToFile(file_name);
     }
 
     void writeToFile(string filename){
@@ -126,21 +97,21 @@ class Image
 };
 
 int main(int argc, char** argv) {
-    if(argc < 3){
-        cout << "usage: ./a.out <path to input file> <path to output file>\n";
+    if(argc < 2){
+        cout << "usage: ./a.out <path to output file>\n";
         return 0;
     }
-    // Read Image
-    Image image(argv[1]);
-    image.file_name = argv[2];
+    
+    read_sem = sem_open(SNAME_READ, 0);
+    write_sem = sem_open(SNAME_WRITE, 0);
+
+    Image image;
+
+    // Get file name
+    image.file_name = argv[1];
     
     // Transform
-    thread gray_thread( &Image::grayScale, &image );
-
-    thread edge_thread( &Image::detectEdges, &image );
-
-    gray_thread.join();
-    edge_thread.join();
+    image.detectEdges();
 
     return 0;
 }
--- lab5/transform_3gray.cpp
+++ lab5/transform_3gray.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <string.h>
 #include <vector>
-#include <thread>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <semaphore.h>
@@ -11,21 +10,21 @@
 #define MAX 4096
 #define vvi vector<vector<int>>
 #define vi vector<int>
-#define SNAME "/imagesem"
+#define SNAME_READ "/readimage"
+#define SNAME_WRITE "/writeimage"
 
 using namespace std;
 
-sem_t *sem = sem_open(SNAME, O_CREAT, 0644, 0);  // Init value 0
+sem_t *write_sem;
+sem_t *read_sem;
 
 class Image
 {
   public:
-    int** gray;
     int ** r;
     int ** g;
     int ** b;
-    int ** edges;
-    int h, w, new_h, new_w;
+    int h, w;
     string file_name;
 
     Image(){
@@ -51,14 +50,10 @@ class Image
         fin >> val;
 
         // Dynamically allocate all 2D Arrays
-        gray = new int*[h];
-        edges = new int*[h];
         r = new int*[h];
         g = new int*[h];
         b = new int*[h];
         for(int i=0; i<h; ++i){
-            gray[i] = new int[w];
-            edges[i] = new int[w];
             r[i] = new int[w];
             g[i] = new int[w];
             b[i] = new int[w];
@@ -74,54 +69,20 @@ class Image
     }
 
     void grayScale() {
-        for(int i=0; i < h; ++i) {
-            for(int j = 0; j < w; ++j){
-                gray[i][j] = 0.3*r[i][j] + 0.59*g[i][j] + 0.11*b[i][j];
-            }
-        }
-    }
+        key_t my_key = ftok("shmfile", 65); // ftok function is used to generate unique key
+        int shmid = shmget(my_key, sizeof(int), 0666|IPC_CREAT); // shmget returns an ide in shmid
+        int *pixel = (int*) shmat(shmid,(void*)0,0); // shmat to join to shared memory
 
-
-    void detectEdges() {
-        int filter[3][3] = {
-            {1, 2, 1},
-            {2, -13, 2},
-            {1, 2, 1}
-        };
-
-        new_h = h - 2;
-        new_w = w - 2;
-
-        for(int i = 0; i < h; ++i){
-            for(int j = 0; j < w; ++j){
-                int pixel = 0;
-                for(int x = 0; x<3; ++x){
-                    for(int y = 0; y<3; ++y){
-                        if(i + x >= h || j + y >= w)
-                            continue;
-                        pixel += gray[i+x][j+y] * filter[x][y];
-                    }
-                }
-                edges[i][j] = max(min(pixel, 255), 0);
+        for(int i=0; i < 10; ++i) {
+            for(int j = 0; j < 10; ++j){
+                sem_wait(read_sem);
+                *pixel = 0.3*r[i][j] + 0.59*g[i][j] + 0.11*b[i][j];
+                cout << *pixel << "\n";
+                sem_post(write_sem);
             }
         }
 
-        writeToFile(file_name);
-    }
-
-    void writeToFile(string filename){
-        //writeToFile
-        ofstream fout(filename);
-
-        fout << "P2\n";
-        fout << w << " " << h << "\n";
-        fout << 255 << "\n";
-        for(int i=0; i < h; ++i){
-            for(int j = 0; j < w; ++j){
-                fout << edges[i][j] << "\n";
-            }
-        }
-        fout.close();
+        shmdt(pixel);
     }
 };
 
@@ -130,17 +91,20 @@ int main(int argc, char** argv) {
         cout << "usage: ./a.out <path to input file> <path to output file>\n";
         return 0;
     }
+
+    write_sem = sem_open(SNAME_WRITE, O_CREAT, 0644, 0);  // Init value 0
+    read_sem = sem_open(SNAME_READ, O_CREAT, 0644, 1); // Init value 1    
+    
+
     // Read Image
     Image image(argv[1]);
     image.file_name = argv[2];
     
     // Transform
-    thread gray_thread( &Image::grayScale, &image );
-
-    thread edge_thread( &Image::detectEdges, &image );
-
-    gray_thread.join();
-    edge_thread.join();
+    image.grayScale();
+    
+    //detach from shared memory
+    
 
     return 0;
 }
