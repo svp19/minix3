3d849fe040d7e77d1a4c4bcbb97393a4eccb40f7
svp19 <170010025@iitdh.ac.in>
2020-03-13 19:15:14 +0530

    Lab6

--- lab6/run.sh
+++ lab6/run.sh
@@ -1,4 +1,5 @@
-cp ./open.c /usr/src/minix/servers/vfs/open.c
-cp ./read.c /usr/src/minix/servers/vfs/read.c
-cp ./link.c /usr/src/minix/servers/vfs/link.c
-make build MKUPDATE=yes >log.txt 2>log.txt
+cp ./open.c /usr/src/minix/servers/vfs/open.c;
+cp ./read.c /usr/src/minix/servers/vfs/read.c;
+cp ./link.c /usr/src/minix/servers/vfs/link.c;
+cd /usr/src;
+make build MKUPDATE=yes >log.txt 2>log.txt;
