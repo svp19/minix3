a670f211d54ad7edb0f5f433fe4e2b3d95bdc0ca
svp19 <170010025@iitdh.ac.in>
2020-04-07 10:09:23 +0530

    ls wasn't working

--- minix/lib/libc/gen/fslib.c
+++ minix/lib/libc/gen/fslib.c
@@ -49,6 +49,7 @@ int block_size;
 uint8_t fs_mode_to_type(mode_t mode)
 {
 	if(S_ISREG(mode)) return DT_REG;
+	else if(S_ISIMM(mode)) return DT_REG;	
 	else if(S_ISDIR(mode)) return DT_DIR;
 	else if(S_ISLNK(mode)) return DT_LNK;
 	else if(S_ISCHR(mode)) return DT_CHR;
